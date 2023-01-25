b54a46644d26b4d48a0f5708b6877adb3c3fd07b
EyeOfCode <champuplove@gmail.com>
2023-01-25 12:19:08 +0700

    feat: add example create data
    Signed-off-by: EyeOfCode <champuplove@gmail.com>

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,24 +1,41 @@
 import { useEffect, useState } from 'react';
 import Api from '@generators/api';
+import { CreateAppDto } from '@generators/type';
 
 interface IApp {
   name: string;
 }
 
 export function App() {
+  const connect = new Api();
   const [text, setText] = useState<IApp[]>();
+  const [name, setName] = useState<string>();
 
   useEffect(() => {
-    const connect = new Api();
-    connect.AppControllerGetData().subscribe((res) => setText(res));
+    fetchData();
   }, []);
+
+  const fetchData = () => {
+    connect.AppControllerGetData().subscribe((res) => setText(res));
+  };
+
+  const create = () => {
+    if (name) {
+      const data: CreateAppDto = { name };
+      connect.AppControllerCreateData(data).subscribe(() => fetchData());
+    }
+  };
 
   return (
     <div>
       <div>Welcome test gen api</div>
+      <div style={{ display: 'flex' }}>
+        Input: <input onChange={(res) => setName(res.target.value)} />
+        <button onClick={create}>submit</button>
+      </div>
       {text?.length ? <div>::List App::</div> : null}
       {text?.map((data) => (
-        <div>{data.name}</div>
+        <div key={data.name}>{data.name}</div>
       ))}
     </div>
   );
