3e525edd0bd89ff85d05c19be1c4839ab36f2c12
EyeOfCode <champuplove@gmail.com>
2023-01-26 17:06:10 +0700

    chore: update igore lint
    Signed-off-by: EyeOfCode <champuplove@gmail.com>

--- generations/swagger-generator.js
+++ generations/swagger-generator.js
@@ -1,14 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { generateApi } = require('swagger-typescript-api');
-// eslint-disable-next-line @typescript-eslint/typedef,@typescript-eslint/no-var-requires
 const fs = require('fs');
-// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/typedef
 const path = require('path');
-// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/typedef
 const rimraf = require('rimraf');
 require('dotenv').config();
 
-// eslint-disable-next-line @typescript-eslint/typedef
 const folderPath = path.join(
   __dirname,
   `${process.env.NX_DIRECTORY_GENERATE_API || '../libs/generators-api'}`
@@ -28,11 +23,8 @@ generateApi({
   modular: true,
   enumNamesAsValues: false,
   templates: path.join(__dirname, './templates/modular'),
-  // eslint-disable-next-line @typescript-eslint/typedef
 }).then(({ files }) => {
-  // eslint-disable-next-line @typescript-eslint/typedef]
   files.forEach(({ content, name }) => {
-    // eslint-disable-next-line @typescript-eslint/typedef
     const kebabCase = name
       .replace(/([a-z])([A-Z])/g, '$1-$2')
       .replace(/\s+/g, '-')
